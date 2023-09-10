41572b683998bf7c66a8894223cd5d107d55fffb
JahanaSultan <cahana.aliyeva@gmail.com>
2023-09-10 19:48:40 +0400

    update

--- setting/settings.py
+++ setting/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-mnx)navcbdt3offu_a@2@j9ivex36^d#f9i=tcrspf9m$wd1a9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['jahanasultan.pythonanywhere.com', "127.0.0.1"]
 
 
 # Application definition
